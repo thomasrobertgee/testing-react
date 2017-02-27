2d3c8a181c1e5143af99e080e4a196e5f5deae26
Thomas Gee <thomasrobertgee@gmail.com>
2017-02-27 21:25:49 +1100

    implemented more testing for react components, moving on to testing for redux

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react'
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { comment: ''}
+  }
+
+
+  handleChange(event) {
+    this.setState({ comment: event.target.value })
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    this.setState({ comment: ''})
+  }
+
   render() {
     return (
-      <div className="comment-box">
-        <textarea />
-        <button>Submit Button</button>
-      </div>
+      <form onSubmit={this.handleSubmit.bind(this)} className="comment-box">
+        <textarea
+          value={this.state.comment}
+          onChange={this.handleChange.bind(this)} />
+        <button action="submit">Submit Button</button>
+      </form>
     )
   }
 }
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,19 +1,37 @@
 import { renderComponent, expect } from '../test_helper'
 import App from '../../src/components/app'
 
-// use 'describe' to group together similar tests
-// can be named whatever, good practice to name it the same name as the app you are testing
+// // use 'describe' to group together similar tests
+// // can be named whatever, good practice to name it the same name as the app you are testing
+// describe('App', () => {
+//
+//   // use 'it' to test a single attribute of a target
+//   it('shows the correct text', () => {
+//
+//     // create an instance of App
+//     const component = renderComponent(App)
+//
+//     // use 'expect' to make an 'assertion' about a target
+//     expect(component).to.contain('React simple starter')
+//
+//   })
+//
+// })
+
 describe('App', () => {
+  let component
+
+  beforeEach(() => {
+    component = renderComponent(App)
+  })
 
   // use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist
+  })
 
-    // create an instance of App
-    const component = renderComponent(App)
-
-    // use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter')
-
+  it('shows a comment list', () => {
+    expect(component.find('.comment-list')).to.exist
   })
 
 })
